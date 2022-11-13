ea5f4de581183ae8470a3837f06424eb673bfd1d
unknown <you@example.combagaseptian26@gmail.com>
2022-11-13 15:09:11 +0700

    first commit

--- components/CarouselItem.tsx
+++ components/CarouselItem.tsx
@@ -1,27 +1,9 @@
-import React, { useCallback, useContext } from "react";
-import { CarouselContext } from "./Carousel";
-import styles from "../styles/carousel.module.css";
+import React from 'react'
 
-interface Props {
-  index: number;
-  children: JSX.Element;
+const CarouselItem = () => {
+  return (
+    <div>CarouselItem</div>
+  )
 }
 
-const CarouselItem = ({ children, index }: Props) => {
-  const { embla: emblaApi, selectedIndex } = useContext(CarouselContext);
-  const isActive = selectedIndex === index;
-  const handleClick = useCallback(() => {
-    if (!emblaApi) return;
-    emblaApi.scrollTo(index);
-  }, [emblaApi, index]);
-  return (
-    <div
-      className={`${styles.slide} relative ${isActive ? "active" : ""}`}
-      onClick={handleClick}
-    >
-      {children}
-    </div>
-  );
-};
-
-export default CarouselItem;
+export default CarouselItem
--- components/TeamSlider.tsx
+++ components/TeamSlider.tsx
@@ -1,45 +1,9 @@
-import React from "react";
-import Carousel from "./Carousel";
-import CarouselItem from "./CarouselItem";
+import React from 'react'
 
 const TeamSlider = () => {
   return (
-    <Carousel className="py-10 my-10 text-white bg-[#0099ff] rounded-tl-[100px] rounded-br-[100px] lg:py-20">
-      <CarouselItem index={0}>
-        <div className="flex flex-col items-center justify-center text-center">
-          <div className="flex items-center justify-center py-4">
-            <img
-              src="/assets/doc.jpg"
-              alt="Murat"
-              className="w-[120px] h-[180px] "
-            />
-          </div>
-          <h1 className="pb-4 text-3xl">Exp. Dr. Murat SENER</h1>
-          <h2 className="pb-4 text-xl">Specialist Cardiology</h2>
-          <p>
-            Dr. Murat Sener berkerja sebagai "Specialist Cardiology di Florence Nightingale Hospital"
-          </p>
-        </div>
-      </CarouselItem>
-      <CarouselItem index={1}>
-        <div className="flex flex-col items-center justify-center text-center">
-          <div className="flex items-center justify-center py-4 ">
-            <img
-              src="/assets/team/Mark-Prichard-e1405584354538.png"
-              alt=""
-              className="w-[100px] h-[100px]"
-            />
-          </div>
-          <h1 className="pb-4 text-3xl">Dr. Zahed Parvez</h1>
-          <h2 className="pb-4 text-xl">Specialist Cardiology</h2>
-          <p>
-            
-          </p>
-        </div>
-      </CarouselItem>
-     
-    </Carousel>
-  );
-};
+    <div>TeamSlider</div>
+  )
+}
 
-export default TeamSlider;
+export default TeamSlider
